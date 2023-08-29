0512ae528c040d38ced5da59ace346beea58e1a6
bob <robert.matthew.brooks@gmail.com>
2023-08-29 09:12:08 +0100

    responsive layout changes

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,57 @@
+import { useState } from 'react';
 import Logo from './HeaderLogo.jsx';
+import menuOpenImg from './assets/menu-open.svg';
+import menuCloseImg from './assets/menu-close.png';
 import './Header.css';
 
 const menuItems = [
+  { text: 'Home', link: './' },
   { text: 'Skills', link: '#' },
   { text: 'Portfolio', link: './portfolio' },
   { text: 'Contact', link: './contact' },
 ];
 
 export default function Header() {
+  const [isMobileNavVisible, setIsMobileNavVisible] = useState(false);
+
+  function toggleMenu() {
+    if (!isMobileNavVisible) {
+      window.scrollTo(0, 0);
+      document.body.style.overflowY = 'hidden';
+    } else {
+      document.body.style.overflowY = 'scroll';
+    }
+    setIsMobileNavVisible(!isMobileNavVisible);
+  }
+
   return (
-    <header className="Header">
+    <header id="Header">
       <Logo />
-      <nav className="desktop-nav">
+
+      <button id="Header__mobile-nav-btn" onClick={() => toggleMenu()}>
+        <img src={isMobileNavVisible ? menuCloseImg : menuOpenImg} />
+      </button>
+
+      <nav
+        id="Header__mobile-nav"
+        style={{ left: isMobileNavVisible ? '0' : '-100vw' }}
+        onClick={(e) => {
+          // don't toggle menu on link clicked, only on bg clicked
+          if (e.target === e.currentTarget) {
+            toggleMenu();
+          }
+        }}
+      >
+        {menuItems.map((menuItem, i) => {
+          return (
+            <a key={i} href={menuItem.link}>
+              {menuItem.text}
+            </a>
+          );
+        })}
+      </nav>
+
+      <nav className="Header__desktop-nav">
         {menuItems.map((menuItem, i) => {
           return (
             <a key={i} href={menuItem.link}>
--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -8,7 +8,7 @@ const exampleProject = {
   title: 'Kata: Sum Consecutive Digits',
   body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati aliquam eaque modi aperiam cumque sunt, incidunt officiis illo quasi omnis dolorem quia praesentium numquam perferendis ducimus culpa distinctio ipsa magni.',
   img_url:
-    'https://lh3.googleusercontent.com/pw/AIL4fc-PqNdbdRbGNoabPptE4q2yF23Q5uma_2RZif8krFMjJ9cg570XX6-A0-hqiqDL09oS9Fj75x4OUAF5GZ_Lh1IIL6LZpMJLw5qqATwzYJyGzcL80zWd8kzdoQbdLYWhwkeCpUpMYcRcrwFW2WA2-dGtUoXMqjeXD-uqEIWyojbESHKY0V37Q7DSUkMj1e5OMVaH7eTiXf3erQrf-Moyzhe7t_51kHbrkeCe6dV3yX2mRwTkczMmqxX7-_ssZbzOqVxRMfI-dzNwSNctkYakngle7AeGINntIdBZFtzET_m80x3_Tyi2qh2Vw-L5kbvmGrHH-BMIicBXUdbJwzjVUEp3fN1AYAMYVjn364iVg_7enZ0k-iSefmVppMUexslYQjOKvYc85NUk1bfzhI5yiHjzQhMxqd_dKV21VxF3g4PIMdIGXlIHFuM4vt922UBkcizCQu7yQJzTMskT_qZkkfJxh-JYApOWl_-L6PDSWyg0r3OCKm6nS0BJ5Y3M0ux6N90IvlSPNhkqN8-dndH2I6KTtPSHCziSJSnOmIPJIRecXPCm4rHFnPyAxn3yL_WYdzBG7NPmTOfyYtW9Gc-WGx0sVdPTuz9Z1AjhsHA3UvyYWvwt6k-o-a4EA08ectjGJRSIrdNlUqdNI8-xGom8h2H5qaIE8ILc9fUHWy3KIyV_M8YjRY_C_UCDuL7H3F1f-4oU2GGIinHjMIlDTfjTNNE9ESBlk-CJigURb5Upc9r6bnYoJZ_0xlFHS31ieOE3pSf_QVcRHmZUuvABtprurUqiJKvVIyVFN9rOMNl77uQH9-NFSkpXKqxa1N-igxMUjOUQDKfsEQZ9MRe-_iBLssC0chdTE6HmFN-dxOuEXov8cTMwlJrizpS-lqXpG__aXmDkuWXu1fYNlKdNu6FruSryHGfvT9jm44FE5v_sHUgjc_hg4gCWkeDp4WQaBw=w500-h302-s-no',
+    'https://lh3.googleusercontent.com/pw/AIL4fc9G7dHOKn0MGGgShYrKC91PnJ0MYeSpBheOTQNqRWuGMtnnWGyv1wcWBGCDtnfcsUfqFFg6lm3Xn4D8zYmEIK_Tr4wn3jgfr2BQPjto0NUQTdFLMbP8HKulvDOfP062A8rLjKdde582j04j_1h65_Y=w500-h302-s-no?authuser=0',
   video_url: 'https://www.youtube.com/watch?v=feOudajBVOc',
   languageIcons: [
     'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
