7d0346ca95a015ff7e55ca93ea65a17a702ae15d
bob <robert.matthew.brooks@gmail.com>
2023-09-08 13:56:41 +0100

    fix bug with hash link

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,23 @@
+import { useRef, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Skills from './Skills';
 import Splash from './Splash';
 
 export default function Home() {
+  const { hash } = useLocation();
+
+  useEffect(() => {
+    // scroll to #Skills only when page loaded
+    // because #Skills doesn't exist React creates it
+    // cannot link straight to it
+
+    if (hash) {
+      const id = hash.replace('#', '');
+      const element = document.getElementById(id);
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [hash]);
+
   return (
     <>
       <Splash />
