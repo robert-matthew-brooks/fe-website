2811a33c6426b029712c7d7ff4b9655c4180febb
bob <robert.matthew.brooks@gmail.com>
2023-09-18 17:47:52 +0100

    changes to scroll to top button

--- src/ScrollToTopBtn.jsx
+++ src/ScrollToTopBtn.jsx
@@ -5,15 +5,18 @@ export default function ScrollToTopBtn() {
   const [isVisible, setIsVisible] = useState(false);
 
   window.onscroll = () => {
-    if (document.documentElement.scrollTop > 100) {
+    if (!isVisible && document.documentElement.scrollTop > window.innerHeight) {
       setIsVisible(true);
-    } else setIsVisible(false);
+    } else if (isVisible && document.documentElement.scrollTop === 0)
+      setIsVisible(false);
   };
 
   return (
     <button
       id="ScrollToTopBtn"
-      style={{ opacity: isVisible ? 1 : 0 }}
+      className={
+        isVisible ? 'ScrollToTopBtn--visible' : 'ScrollToTopBtn--hidden'
+      }
       onClick={() => {
         window.scrollTo({
           top: 0,
